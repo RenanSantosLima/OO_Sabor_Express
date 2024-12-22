b72dde158ead85f66e96c301047b8d2253be617a
RenanSantosLima <renanclasantoslima@gmail.com>
2024-12-22 15:12:25 -0300

    Polimorfismo e mais herança

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
+from modelos.cardapio.sobremesa import Sobremesa
 
 restaurante_praca = Restaurante('praca', 'Gourmet')
-restaurante_praca.receber_avaliacao('Gui', 10)
-restaurante_praca.receber_avaliacao('Lais', 8)
-restaurante_praca.receber_avaliacao('Emy', 2)
+bebida_suco = Bebida('Suco de melancia', 5.0, 'grande')
+bebida_suco.aplicar_desconto()
+prato_paozinho = Prato('Paozinho', 2.00, 'O melhor da cidade')
+prato_paozinho.aplicar_desconto()
+sobremesa_picole = Sobremesa('Picole', 2.00, 'palito', 'medio', 'O mehor picole da cidade')
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
+
 
 def main():
-    Restaurante.listar_restaurantes()
+    restaurante_praca.exibir_cardapio
 
 if __name__ == '__main__':
     main()
